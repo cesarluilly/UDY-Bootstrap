beb70fdd07cd7e4b05b7292abd7ff789b8ad8afd
cesarluilly <cesarluilly.garcia@hotmail.com>
2022-12-01 19:16:04 -0600

    Moving components to the module practica-bootstrap5

--- TestBootstrap/src/app/practicas-bootstrap5/practicas-bootstrap5.module.ts
+++ TestBootstrap/src/app/practicas-bootstrap5/practicas-bootstrap5.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TestCesarComponent } from './test-cesar/test-cesar.component';
+
 
 
 
 @NgModule({
   declarations: [
-    TestCesarComponent
   ],
   imports: [
     CommonModule
